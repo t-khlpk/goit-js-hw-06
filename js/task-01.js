7fd5cd5b7375530d5642852dd0865f02402b63b2
t-khlpk <110490094+t-khlpk@users.noreply.github.com>
2023-02-20 23:37:01 +0200

    Update task-01.js
    task-01

---
+++ js/task-01.js
@@ -0,0 +1,24 @@
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const allCategories = document.querySelectorAll(".item");
+console.log(`Number of categories:`, allCategories.length);
+
+const allTitlesEl = document.querySelectorAll("h2");
+
+allTitlesEl.forEach((title) => {
+    console.log(`Category: `, title.textContent);
+    console.log(`Elements: `, title.nextElementSibling.children.length);
+});
